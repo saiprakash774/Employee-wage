f57032edc5448b3de781c9595a2e500079d66063
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-09 17:59:02 +0530

    Function for Working Hour

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,8 @@
-#!/bin/bash -x
-
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-numworkingdays=20;
-totalworkingdays=0;
-totalemphr=0;
-max_hrs_in_month=100;
-
-while [[ $totalemphr -lt $max_hrs_in_month && $totalworkingdays -lt $numworkingdays ]]
-do
-
-                 ((totalworkingdays++))
-                empCheck=$(( RANDOM%3 ));
-                case $empCheck in
-                        $isFullTime)
-                                empHrs=8
-                                ;;
-                        $isPartTime)
-                                empHrs=4
-                                ;;
-
-
-                                *)
-                                empHrs=0
-                                ;;
-                esac
-                totalemphr=$(( $totalemphr+$empHrs ));
-done
-                totalSalary=$(( $totalemphr*$empRatePerHr ));
+#!/bin/bash
+function calculateDailyEmployeeWage(){
+        read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+        read -p "To full day hours is :" isFullDayHour
+        calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+        echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+}
+calculateDailyEmployeeWage
