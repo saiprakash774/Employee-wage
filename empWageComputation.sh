cd4e8937bac7ca8a847e7ddf75907d51f6330e63
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-09 17:47:25 +0530

    Employee Wage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,27 @@
 #!/bin/bash -x
+
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=100;
-randomCheck=$((RANDOM%3));
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
 
-case $randomCheck in
-        $isFullTime)
-                    empHrs=12;
-                       ;;
-        $isPartTime)
-                    empHrs=8;
-                       ;;
-               *)
-                    empHrs=0;
-esac
-salary=$(( $empRatePerHr*$empHrs ));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+        empCheck=$((RANDOM%3));
+                case $empCheck in
+                        $isFullTime)
+                                empHrs=8
+                                ;;
+                        $isPartTime)
+                                empHrs=4
+                                ;;
+
+                                  *)
+                                empHrs=0
+                                ;;
+                esac
+                salary=$(($empHrs*$empRatePerHr));
+                totalSalary=$(($totalSalary+$salary));
+done
+echo "Total salary for a month is" $totalSalary
