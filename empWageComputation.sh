3e07fd114e49acabe1f816121fd4cb38fda119c9
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-09 17:40:03 +0530

    Employee Wage using Switchcase

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 isPartTime=1;
 isFullTime=2;
-wageperhr=20;
-randomCheck=$(( RANDOM%2 +1 ))
-if [ $isFullTime = $randomCheck ]
-then
-        empHrs=12;
-else
-        empHrs=8;
-fi
-salary=$(($wageperhr*$empHrs));
+empRatePerHr=100;
+randomCheck=$((RANDOM%3));
+
+case $randomCheck in
+        $isFullTime)
+                    empHrs=12;
+                       ;;
+        $isPartTime)
+                    empHrs=8;
+                       ;;
+               *)
+                    empHrs=0;
+esac
+salary=$(( $empRatePerHr*$empHrs ));
